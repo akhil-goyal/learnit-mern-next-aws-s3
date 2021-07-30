cdcbd02cc7d3857696dc4cd0d73716ad88e7a9ef
Akhil Goyal <akhilgoyal@akhils-mbp.lan>
2021-07-30 13:54:56 -0400

    Setup the routes, controllers & middlewares.

---
+++ server/routes/auth.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const router = express.Router();
+
+// Controllers
+import { register } from './../controllers/auth';
+
+router.get('/register', register);
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,1 +1,26 @@
-console.log('Server Setup!');
+import express from 'express';
+import cors from 'cors';
+import { readdirSync } from 'fs';
+
+// Morgan has some issue with import syntax.
+const morgan = require('morgan');
+require('dotenv').config();
+
+// Express Application
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+// ROUTES - Importing all the routes dynamically so that
+// we do not have to import them manually.
+readdirSync('./routes').map((r) => app.use('/api', require(`./routes/${r}`)));
+
+// PORT
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT : ${PORT}`);
+});
