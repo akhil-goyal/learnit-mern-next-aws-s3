2c5b238e0ca671237c21440b86a706133aff7a47
Akhil Goyal <akhilgoyal@akhils-mbp.lan>
2021-07-30 12:40:22 -0400

    Configured Next.js & added bootstrap, Ant Design.

---
+++ client/pages/index.js
@@ -0,0 +1,10 @@
+const Index = () => {
+    return (
+        <>
+            <h1 className="jumbotron text-center bg-primary">Hello World!</h1>
+            <p>FROM NEXT.JS</p>
+        </>
+    )
+}
+
+export default Index;
