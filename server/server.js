d41e560331f5c82c8716bea1026276a3adfb2353
Akhil Goyal <akhilgoyal@akhils-mbp.lan>
2021-07-31 13:09:06 -0400

    Enhanced the protection of HTTP requests from CSRF attacks.

--- server/server.js
+++ server/server.js
@@ -2,10 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import { readdirSync } from 'fs';
+import csrf from 'csurf';
+import cookieParser from 'cookie-parser'
 
 // Morgan has some issue with import syntax.
 const morgan = require('morgan');
 require('dotenv').config();
+
+const csrfProtection = csrf({ cookie: true });
 
 // Express Application
 const app = express();
@@ -26,11 +30,19 @@ mongoose
 // Middlewares
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 app.use(morgan('dev'));
 
 // ROUTES - Importing all the routes dynamically so that
 // we do not have to import them manually.
 readdirSync('./routes').map((r) => app.use('/api', require(`./routes/${r}`)));
+
+// CSRF Protection
+app.use(csrfProtection)
+
+app.get('/api/csrf-token', (req, res) => {
+    res.json({ csrfToken: req.csrfToken() });
+});
 
 // PORT
 const PORT = process.env.PORT || 8000;
