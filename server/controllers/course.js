73290ed702a2e95fd58029ad5c587086e97d927c
Akhil Goyal <akhilgoyal@akhils-mbp.lan>
2021-08-03 13:04:03 -0400

    Setup the server-side API to upload course lessons to Amazon s3 bucket.

--- server/controllers/course.js
+++ server/controllers/course.js
@@ -2,6 +2,7 @@ import AWS from 'aws-sdk';
 import slugify from 'slugify';
 import { nanoid } from 'nanoid';
 import Course from './../models/course';
+import { readFileSync } from 'fs';
 
 const awsConfig = {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -114,6 +115,40 @@ export const read = async (req, res) => {
         const course = await Course.findOne({ slug: req.params.slug }).populate('instructor', '_id name').exec();
 
         res.json(course);
+
+    } catch (err) {
+        console.log(err);
+    }
+
+}
+
+export const uploadVideo = async (req, res) => {
+
+    try {
+
+        const { video } = req.files;
+        console.log(video);
+
+        if (!video) return res.status(400).send('No video!');
+
+        // Video params
+        const params = {
+            Bucket: 'learnit-mern-bucket',
+            Key: `${nanoid()}.${video.type.split('/')[1]}`,
+            Body: readFileSync(video.path),
+            ACL: 'public-read',
+            ContentType: video.type,
+        }
+
+        // Upload to S3
+        S3.upload(params, (err, data) => {
+            if (err) {
+                console.log(err);
+                res.sendStatus(400);
+            }
+            console.log(data);
+            res.send(data);
+        });
 
     } catch (err) {
         console.log(err);
--- server/routes/course.js
+++ server/routes/course.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import formidable from 'express-formidable';
 
 const router = express.Router();
 
@@ -6,11 +7,12 @@ const router = express.Router();
 import { requireSignIn, isInstructor } from './../middlewares';
 
 // Controllers
-import { uploadImage, removeImage, create, read } from './../controllers/course';
+import { uploadImage, removeImage, create, read, uploadVideo } from './../controllers/course';
 
 router.post('/course/upload-image', uploadImage);
 router.post('/course/remove-image', removeImage);
 router.post('/course', requireSignIn, isInstructor, create);
 router.get('/course/:slug', read);
+router.post('/course/video-upload', requireSignIn, formidable(), uploadVideo);
 
 module.exports = router;
